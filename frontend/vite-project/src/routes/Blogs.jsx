713948ae20751cc23c1a0a23fffdc5b8b4dd22ea
MrPotatorust <bumko96@gmail.com>
2024-09-19 20:09:52 +0200

    Finished blog just styling to be done

--- frontend/vite-project/src/routes/Blogs.jsx
+++ frontend/vite-project/src/routes/Blogs.jsx
@@ -2,40 +2,38 @@ import { useEffect, useState } from "react";
 
 function Blogs() {
 
-    const [blogs, setBlogs] = useState();
-    const [blogLen, setBlogLen] = useState(10)
-
-
-
+    const [blogs, setBlogs] = useState([]);
+    const [blogLen, setBlogLen] = useState(10);
+  
     useEffect(() => {
-        setBlogs(getBlogs(0, 10))
-    }, [])
-    
-    
-
+      getBlogs(0, blogLen);
+    }, [blogLen]);
+  
     async function getBlogs(start, end) {
+      try {
         const res = await fetch(`http://127.0.0.1:8000/api/get_blogs/${start}/${end}`);
         const data = await res.json();
-        setBlogs(data)
+        setBlogs(data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
     }
-
-    // const mappedBlogs = blogs.map((blog) => {
-    //     return (
-    //         <div key={blog.id}>
-    //             <h2>{blog.title}</h2>
-    //             <p>{blog.published_date}</p>
-    //             <p>{blog.content}</p>
-    //         </div>
-    //     )
-    // })
-
-    console.log(blogs.len)
-
+  
     return (
-        <div>
-            <h1>Blogs</h1>
-        </div>
+      <>
+        <h1>Blogs</h1>
+        {blogs.map((blog) => (
+          <div key={blog.id} className="blog">
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+            <small>{blog.published_date}</small>
+          </div>
+        ))}
+        <button className="load-more"
+          onClick={() => setBlogLen(blogLen + 10)}
+          >Load More</button>
+      </>
     );
-}
+  };
 
 export default Blogs
